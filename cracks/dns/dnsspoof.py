5b17fb6fa67e73865be0dc3bf66f38222f4b01b4
Ithamar Kaplan <ithamar.kaplan@gmail.com>
2025-12-15 16:17:04 +0200

    added notes because

--- cracks/dns/dnsspoof.py
+++ cracks/dns/dnsspoof.py
@@ -10,11 +10,13 @@ class DNSSpoofCrack(Crack):
     if packet.haslayer(scapy.DNS) and packet.getlayer(scapy.DNS).qr == 1:
       dns_layer = packet.getlayer(scapy.DNS)
       
+      #searching for the A record and decoding domain names
       for i in range(dns_layer.ancount):
         ans = dns_layer.an[i]
         if ans.type == 1:  # A record
           domain = ans.rrname.decode().rstrip('.')
           ip_address = ans.rdata
           
+          #checking decoded domain against trusted domains
           if domain not in self.trustedDomains and ip_address != self.trustedDomains[domain]:
             print("[!] DNS Spoofing detected for domain:", domain, "with IP:", ip_address)
