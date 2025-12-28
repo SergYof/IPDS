f6a90c169b89a10dddaa82a5347a8ad68386bd9c
Sergey <sergojaffe@gmail.com>
2025-12-28 16:23:43 +0200

    Added threaded manager

--- manager.py
+++ manager.py
@@ -1,11 +1,22 @@
-import time
 from typing import List
-from base import Crack
+from cracks.base import Crack
+from scapy.plist import PacketList
+from scapy.sendrecv import sniff
+from threading import Thread
+
+ATTACK_CHECK_INTERVAL = 5   # how frequently the chunks are scanned  
+
 class Manager:
+    def runOnce(self, crack: Crack, packetChunk: PacketList):   # check one packet chunk for one crack type
+        crack.identify(packetList)  # TODO: take action if detected
+
     def start(self, cracks: List[Crack]):
-        print("Starting cracks")
-        for crack in cracks:
-            crack.identify()
-        # keep process alive
+        # keep running
         while True:
-            time.sleep(1)
+            print("Sniffing packets")
+            packetLst = sniff(timeout=ATTACK_CHECK_INTERVAL, quiet=True)    # sniff for some time
+
+            for crack in cracks:
+                # TODO: use threads
+                t = Thread(target=self.runOnce, args=(crack, packetLst))
+                t.run()
